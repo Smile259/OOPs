a15e72e96ac2b03ea66c49668a34631e996ca2a1
Smile259 <smile02006@outlook.com>
2025-12-01 12:13:48 +0530

    Add initial commit

---
+++ Function.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+// Function Definition 
+int add(int a, int b){
+    return a+b;
+}
+
+int main(){
+    int sum = add(5,3);
+    cout<<"Sum: "<<sum<<endl;
+    return 0;
+}
